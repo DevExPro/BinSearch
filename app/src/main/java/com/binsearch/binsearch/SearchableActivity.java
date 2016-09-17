9ec9221e7cb723dc43bb2fb69fa7a1bff622dda1
LilyMiranCox <cox@pdx.edu>
2016-09-17 12:21:39 -0700

    Added the SearchableActivity class

---
+++ app/src/main/java/com/binsearch/binsearch/SearchableActivity.java
@@ -0,0 +1,10 @@
+package com.binsearch.binsearch;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    protected void onCreate() {
+    }
+}
