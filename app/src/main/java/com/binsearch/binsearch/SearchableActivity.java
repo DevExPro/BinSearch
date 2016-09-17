2ce3274e89d78915380b4070cddc0d3cebb355db
LilyMiranCox <cox@pdx.edu>
2016-09-17 15:46:41 -0700

    Messing with SearchableActivity class

--- app/src/main/java/com/binsearch/binsearch/SearchableActivity.java
+++ app/src/main/java/com/binsearch/binsearch/SearchableActivity.java
@@ -1,5 +1,7 @@
 package com.binsearch.binsearch;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +11,17 @@ public class SearchableActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+        }
     }
 }
