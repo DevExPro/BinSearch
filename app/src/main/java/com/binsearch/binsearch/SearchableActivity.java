5ccbc041ab5a54402138c7b273461fbcd706e795
Gabriel <gabrieledwards@ymail.com>
2016-09-18 12:06:23 -0700

    some change

--- app/src/main/java/com/binsearch/binsearch/SearchableActivity.java
+++ app/src/main/java/com/binsearch/binsearch/SearchableActivity.java
@@ -1,10 +1,26 @@
 package com.binsearch.binsearch;
 
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class SearchableActivity extends AppCompatActivity {
 
-    protected void onCreate() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    protected void handleIntent(Intent intent){
+        if(Intent.ACTION_SEARCH.equals(
+                intent.getAction())){
+            String query = intent.getStringExtra(
+                    SearchManager.QUERY);
+
+        }
     }
 }
