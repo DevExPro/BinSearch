bf48394a7856e5e67a2c9ae8dffd1896b05afb53
LilyMiranCox <cox@pdx.edu>
2016-09-18 15:51:19 -0700

    Trying to get searchable called

--- app/src/main/java/com/binsearch/binsearch/SearchResult.java
+++ app/src/main/java/com/binsearch/binsearch/SearchResult.java
@@ -1,7 +1,10 @@
 package com.binsearch.binsearch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SearchResult extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class SearchResult extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+
+        Toast.makeText(getApplicationContext(), "Reached the SearchResult class", Toast.LENGTH_LONG).show();
+
+        Intent gatheredIntent = getIntent(); // Get the intent that was passed into the activity
+        String [] searchReceived = gatheredIntent.getStringArrayExtra("foundItem"); // Take the array of item info out of the intent
+
+        // Set connection to TextViews that will hold the received information
+        TextView itemNum = (TextView)findViewById(R.id.NumberVal);
+        TextView itemLocate = (TextView)findViewById(R.id.LocationVal);
+        TextView itemDescript = (TextView)findViewById(R.id.DescriptionVal);
+
+        // Set the TextViews to to the received information
+        itemNum.setText(searchReceived[0]);
+        itemLocate.setText(searchReceived[1]);
+        itemDescript.setText(searchReceived[2]);
     }
 }
